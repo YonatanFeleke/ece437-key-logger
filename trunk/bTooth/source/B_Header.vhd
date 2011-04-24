c84868be22ef45ce70e4c54c4282b740a324362d
yoniaberra <yoniaberra@2621b4e6-58e3-4275-2e42-46601fe0754d>
2011-04-24 05:24:22 +0000

    Encode Packet with monitor seems to be working. Updated odp. For toplevel preparation the blocks have been initialized with assumed signal names.

--- trunk/bTooth/source/B_Header.vhd
+++ trunk/bTooth/source/B_Header.vhd
@@ -11,10 +11,12 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
 ENTITY B_Header IS
+		generic(WAITREG	:	natural := 5264 ); -- time to transmit 8 bits
 		port(		CLK 						:		in	std_logic;
 						RST							:		in	std_logic;
 						HEADER_EN				:		in	std_logic;
 						HEADER					:		OUT	std_logic_vector(53 downto 0);
+						TRANS_EN				:		OUT	std_logic;
 						ENCODE_EN				:		OUT	std_logic);
 END B_Header;
 
@@ -29,6 +31,7 @@ architecture b_hdr of B_Header is
   signal HEC: std_logic_vector(7 downto 0);		--8-bit header error checksum with polynomial 0 1 2 5 7 8
   signal HEADER_1:	std_logic_vector(53 downto 0);
 begin
+--__________________________________________________________
 		getHECprocess : process (CLK,RST)
 --		variable HEC: std_logic_vector(7 downto 0);		--8-bit header error checksum with polynomial 0 1 2 5 7 8
     variable D: std_logic_vector(9 downto 0);
@@ -49,23 +52,40 @@ begin
     HEC(5) <= d(6) xor d(5) xor d(4) xor d(3) xor d(2) xor d(1) xor d(0) xor c(0) xor c(1) xor c(2) xor c(3) xor c(4);
     HEC(6) <= d(7) xor d(6) xor d(5) xor d(4) xor d(3) xor d(2) xor d(1) xor c(0) xor c(1) xor c(2) xor c(3) xor c(4) xor c(5);
     HEC(7) <= d(7) xor d(5) xor d(3) xor d(1) xor d(0) xor c(1) xor c(3) xor c(5);
-		end process getHECprocess;
-		HEADER_1 <= HEC(7)&HEC(7)&HEC(7)&HEC(6)&HEC(6)&HEC(6)&HEC(5)&HEC(5)&HEC(5)&HEC(4)&HEC(4)&HEC(4)&HEC(3)&HEC(3)&HEC(3)&HEC(2)&HEC(2)&HEC(2)&HEC(1)&HEC(1)&HEC(1)&HEC(0)&HEC(0)&HEC(0)&SEQN&SEQN&SEQN&ARQN&ARQN&ARQN&FLOW&FLOW&FLOW&PTYPE(3)&PTYPE(3)&PTYPE(3)&PTYPE(2)&PTYPE(2)&PTYPE(2)&PTYPE(1)&PTYPE(1)&PTYPE(1)&PTYPE(0)&PTYPE(0)&PTYPE(0)&AM_ADDR(2)&AM_ADDR(2)&AM_ADDR(2)&AM_ADDR(1)&AM_ADDR(1)&AM_ADDR(1)&AM_ADDR(0)&AM_ADDR(0)&AM_ADDR(0);							
-outputheader : process (CLK,RST,HEADER_EN)	
+		end process getHECprocess;			
+--___________________________________________________________--
+		outputheader : process (CLK,RST,HEADER_EN)	
+		variable waitcnt	: integer range 0 to WAITREG*16;
+		variable waittx		:	std_logic;
     begin 
       if ( RST = '1') then-- on reset what is the stop
 					HEADER <= "000000000000000000000000000000000000000000000000000000";
-					ENCODE_EN <= '0';					
+					TRANS_EN <= '0';
+					ENCODE_EN<= '0';		
+					waitcnt := 0;
+					waittx	:= '0';			
       elsif ( rising_edge(clk)) then        
     			 if (HEADER_EN = '1') then
 							HEADER <= HEADER_1;
-    			    ENCODE_EN <='1';
+    			    TRANS_EN <='1';
+    			    waittx	:= '1';
   				else -- value indicating not valid via premable => check on sram accept that all are complements
-  				  ENCODE_EN <= '0';
-	      end if;
-      end if;    
+  				  TRANS_EN <= '0';
+  				  if (waittx = '1') then
+  				  	if (waitcnt = WAITREG*16) then -->>>>>>>>>>>>> 126 bits to transmit should be 128???????
+  				  		waitcnt := 0;
+  				  		ENCODE_EN <= '1';
+  				  		waittx := '0';
+  				  	else
+  				  		waitcnt := waitcnt + 1;
+  				  	end if;-- waitcnt
+  				  else
+  				  	ENCODE_EN <= '0';
+  				  end if; -- waittx  				  
+	      end if;-- header_EN
+      end if;-- RST CLK
     end process outputheader;
-    
+--____________________________________________________________--    
     EDGElogic: Process(clk, RST)
 		begin
 			
@@ -83,6 +103,5 @@ outputheader : process (CLK,RST,HEADER_EN)
 				end if;
 			end if;			
 		end process EDGElogic;
+HEADER_1 <= HEC(7)&HEC(7)&HEC(7)&HEC(6)&HEC(6)&HEC(6)&HEC(5)&HEC(5)&HEC(5)&HEC(4)&HEC(4)&HEC(4)&HEC(3)&HEC(3)&HEC(3)&HEC(2)&HEC(2)&HEC(2)&HEC(1)&HEC(1)&HEC(1)&HEC(0)&HEC(0)&HEC(0)&SEQN&SEQN&SEQN&ARQN&ARQN&ARQN&FLOW&FLOW&FLOW&PTYPE(3)&PTYPE(3)&PTYPE(3)&PTYPE(2)&PTYPE(2)&PTYPE(2)&PTYPE(1)&PTYPE(1)&PTYPE(1)&PTYPE(0)&PTYPE(0)&PTYPE(0)&AM_ADDR(2)&AM_ADDR(2)&AM_ADDR(2)&AM_ADDR(1)&AM_ADDR(1)&AM_ADDR(1)&AM_ADDR(0)&AM_ADDR(0)&AM_ADDR(0);		
 end b_hdr;
-
-		
